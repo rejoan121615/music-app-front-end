3c7a8ee2e09039a0f417d1138862b5b414bb4acd
Mohd Rejoan <wearecrx@gmail.com>
2022-04-12 21:08:17 +0600

    navbar complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,17 @@ module.exports = {
         extend: {
             colors: {
                 "dark-blue": "#051120",
+                "dark-blue-900": "#020b16",
                 milk: "#dadada",
+                "milk-dark": "#B3B3B3",
                 border: "rgba(255, 255, 255, 0.07)",
+                yellow: "#E3AA27",
             },
             borderWidth: {
                 1: "1px",
+            },
+            fontFamily: {
+                avenir: "avenir",
             },
         },
     },
