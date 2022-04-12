8a6028c9dc8e80b96d48701a4fb5bc297bd33afe
Mohd Rejoan <wearecrx@gmail.com>
2022-04-13 02:32:37 +0600

    animated navbar created

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log("running");
+
+const menuBtn = document.getElementById("menu-btn");
+const NavBar = document.getElementById("left-navigation");
+
+menuBtn.onclick = function () {
+    if (menuBtn.checked) {
+        NavBar.style.left = "0%";
+    } else {
+        NavBar.style.left = "-100%";
+    }
+};
