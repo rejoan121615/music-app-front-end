2ebaa52f4ccaaf79b5c1498de3ee88861fbae756
Mohd Rejoan <wearecrx@gmail.com>
2022-05-01 14:45:34 +0600

    current-queue and all-songs context menu added

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 const menuBtn = document.getElementById("menu-btn");
 const NavBar = document.getElementById("left-navigation");
 
@@ -11,3 +10,23 @@ menuBtn.onclick = function () {
         NavBar.style.left = "-100%";
     }
 };
+
+// context menu
+const contextWrap = document.querySelector("#context-wrap");
+const contextOptions = document.querySelector("#context-menu");
+
+// context options
+contextWrap.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    contextOptions.style.display = "block";
+    if (window.innerWidth > e.pageX + 420) {
+        contextOptions.style.left = e.pageX + "px";
+        contextOptions.style.top = e.pageY + "px";
+    } else {
+        contextOptions.style.left = window.innerWidth - 420 + "px";
+        contextOptions.style.top = e.pageY + "px";
+    }
+});
+contextWrap.addEventListener("click", () => {
+    contextOptions.style.display = "none";
+});
